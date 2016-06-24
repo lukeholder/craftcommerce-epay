8a61477311d430a55a9963076ae5307c5b8478ee
Luke Holder <lukemh@gmail.com>
2016-06-24 14:04:31 +0800

    init

---
+++ epay/EpayPlugin.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Craft;
+
+class EpayPlugin extends BasePlugin
+{
+
+    private $commerceInstalled = false;
+
+    public function init()
+    {
+        $commerce = craft()->db->createCommand()
+            ->select('id')
+            ->from('plugins')
+            ->where("class = 'Commerce'")
+            ->queryScalar();
+        if($commerce){
+            $this->commerceInstalled = true;
+        }
+    }
+
+    public function getName()
+    {
+        return "Epay Commerce Gateway";
+    }
+
+    /**
+     * Returns the plugin’s version number.
+     *
+     * @return string The plugin’s version number.
+     */
+    public function getVersion()
+    {
+        return "0.1";
+    }
+
+    /**
+     * Returns the plugin developer’s name.
+     *
+     * @return string The plugin developer’s name.
+     */
+    public function getDeveloper()
+    {
+        return "Luke Holder";
+    }
+
+    /**
+     * Returns the plugin developer’s URL.
+     *
+     * @return string The plugin developer’s URL.
+     */
+    public function getDeveloperUrl()
+    {
+        return "#";
+    }
+
+    public function commerce_registerGatewayAdapters(){
+        if($this->commerceInstalled) {
+            require __DIR__ . '/vendor/autoload.php';
+            require_once __DIR__.'/Epay_GatewayAdapter.php';
+            return ['\Commerce\Gateways\Omnipay\Epay_GatewayAdapter'];
+        }
+        return [];
+    }
+}
